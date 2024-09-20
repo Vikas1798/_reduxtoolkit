0e4f07c01fd54b2958a474484bdbe99839dc8a72
Vikas1798 <hkvikas1798@gmail.com>
2024-09-21 00:59:08 +0530

    simple form UI and data listing from store updated

--- src/App.jsx
+++ src/App.jsx
@@ -22,18 +22,22 @@ const App = () => {
     }
 
     return (
-        <>
-            <input type="text" value={name} className="Enter Name" onChange={(e) => setName(e.target.value)} />
-            <button onClick={() => add()}>Add User</button>
-            {
-                storeData?.length ? storeData?.map((d, i) => (
-                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                        <p key={i}>{d}</p>
-                        <button style={{ marginLeft: '30px' }} onClick={() => remove(i)}>Delete</button>
-                    </div>
-                )) : null
-            }
-        </>
+        <div style={{ width: '30%', margin: 'auto', position: "relative" }}>
+            <input style={{ marginBottom: '10px', width: '100%' }} type="text" value={name} placeholder="Enter your Name" onChange={(e) => setName(e.target.value)} />
+            <div style={{ margin: 'auto', width: '40%' }}>
+                <button onClick={() => add()} style={{ width: '100%', margin: 'auto', cursor: 'pointer' }}>Add User</button>
+            </div>
+            <div style={{ marginTop: '20px' }}>
+                {
+                    storeData?.length ? storeData?.map((d, i) => (
+                        <div key={i} style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', border: "1px solid gray", padding: '5px', marginTop: '8px', borderRadius: '4px' }}>
+                            <p style={{ margin: '0' }}>{d}</p>
+                            <button onClick={() => remove(i)} style={{ paddingBlock: '4px', background: 'red', border: 'none', borderRadius: '4px', color: 'white', fontSize: '12px', cursor: 'pointer' }}>Delete</button>
+                        </div>
+                    )) : null
+                }
+            </div>
+        </div>
     )
 }
 
